1224553ae4b3565765f97864180c0c0769047455
Nima Nourinejad <nima.nourinejad@gmail.com>
2024-12-05 16:51:47 +0200

    perpare for using github as host

---
+++ src/components/counter.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: [],
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={this.handleDeccrement}
+          className="btn btn-warning btn-sm ms-2"
+        >
+          Decreament
+        </button>
+        {this.renderTags()}
+      </React.Fragment>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 bg-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+  renderTags() {
+    if (this.state.tags.length === 0) return;
+
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleDeccrement = () => {
+    if (this.state.count === 0) return;
+    this.setState({ count: this.state.count - 1 });
+  };
+}
+
+export default Counter;
